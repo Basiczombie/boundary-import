2c8e2c54b6a26f15d26bc95e696239c24ba939e4
basiczombie <issacgable@gmail.com>
2018-03-27 16:29:33 -0600

    First Commit

---
+++ src/scripts/boundary-import/js/index.js
@@ -0,0 +1,57 @@
+import $ from 'jquery'
+import fetch from 'fetch'
+import fs from 'fs'
+import mime from 'mime-types'
+import DOMParser from 'xmldom'
+import polyline from '@mapbox/polyline'
+import togeo from '@mapbox/togeojson'
+
+// Fetch and place html template
+var pageFragment = fetch('/scripts/boundary-import/html/template.html')
+    .then(response => response.text())
+
+export default function () {
+    Promise.all([pageFragment])
+        .then(function (body) {
+            $('#boundary').closest('tr').before(body)
+
+            let boundaryBox = $('#boundary')
+            let kml = $('#importSchoolBoundary')
+            kml.on('change', function (e) {
+                let fileList = e.target.files[0]
+                let poly = fileProcess(fileList)
+                if (!poly) {
+                    poly = 'This is not a KML format file'
+                } else {
+                    boundaryBox.html(poly)
+                }
+            })
+        })
+}
+
+function fileProcess (kmlFile) {
+    let kmlMimeType = 'application/vnd.google-earth.kml+xml'
+    if (mime.lookup(kmlFile) === kmlMimeType) {
+        return poly(kmlFile)
+    }
+    return false
+}
+
+// Polyline generator
+function poly (kmlFile) {
+    // Polyline variables
+    var kml = new DOMParser.DOMParser().parseFromString(fs.readFileSync(kmlFile, 'utf8'))
+    var converted = togeo.kml(kml)
+    var coords = converted.features[1].geometry.coordinates[0].map(element => {
+        return element.slice(0, 2)
+    })
+
+    return polyline.fromGeoJSON({
+        'type': 'Feature',
+        'geometry': {
+            'type': 'LineString',
+            'coordinates': coords
+        },
+        'properties': {}
+    })
+}
